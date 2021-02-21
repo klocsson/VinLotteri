2d46ff923252d1558547e71dfe2685f735024dac
Michal Kloc <mkloc.developer@gmail.com>
2021-02-21 08:37:43 +0100

    Init repo

---
+++ VinLotteri/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VinLotteri.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
