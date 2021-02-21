3ea26482fba7a406bbc05d5b0eacc2b00edeff2d
Michal Kloc <mkloc.developer@gmail.com>
2021-02-21 11:18:19 +0100

    Add command

--- VinLotteri/Models/Ticket.cs
+++ VinLotteri/Models/Ticket.cs
@@ -2,7 +2,7 @@ namespace VinLotteri.Models
 {
     public class Ticket
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int NrOfTickets { get; set; }
     }
 }
--- VinLotteri/ViewModels/TicketsListViewModel.cs
+++ VinLotteri/ViewModels/TicketsListViewModel.cs
@@ -1,16 +1,44 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reactive;
+using ReactiveUI;
 using VinLotteri.Models;
 
 namespace VinLotteri.ViewModels
 {
     public class TicketsListViewModel : ViewModelBase
     {
+        string name;
+        int numberOfTickets;
+        
         public TicketsListViewModel(IEnumerable<Ticket> tickets)
         {
             Tickets = new ObservableCollection<Ticket>(tickets);
+            var addEnabled = this.WhenAnyValue(
+                t => t.Name,
+                t => t.NumberOfTickets,
+                (s, i) => !string.IsNullOrWhiteSpace(s) && 
+                           !string.IsNullOrWhiteSpace(i.ToString()) && i > 0);
+            
+            AddTicket = ReactiveCommand.Create(
+                () => new Ticket { Name = Name, NrOfTickets = NumberOfTickets}, 
+                addEnabled);
+        }
+
+        public string Name
+        {
+            get => name;
+            set => this.RaiseAndSetIfChanged(ref name, value);
+        }
+
+        public int NumberOfTickets
+        {
+            get => numberOfTickets;
+            set => this.RaiseAndSetIfChanged(ref numberOfTickets, value);
         }
 
         public ObservableCollection<Ticket> Tickets { get; }
+        
+        public ReactiveCommand<Unit, Ticket> AddTicket { get; }
     }
 }
