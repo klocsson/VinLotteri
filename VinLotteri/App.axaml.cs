ed2cd053c427e9788a025232b3c2c447898ad607
Michal Kloc <mkloc.developer@gmail.com>
2021-02-22 19:58:00 +0100

    Drawing animation

--- VinLotteri/App.axaml.cs
+++ VinLotteri/App.axaml.cs
@@ -1,6 +1,9 @@
+using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.Configuration;
 using VinLotteri.Services;
 using VinLotteri.ViewModels;
 using VinLotteri.Views;
@@ -18,8 +21,15 @@ namespace VinLotteri
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                Console.WriteLine(Directory.GetCurrentDirectory());
+
+                var apiKey = configuration["random_api_key"];
                 var db = new Database();
-                var random = new RandomOrg();
+                var random = new RandomOrg(apiKey);
                 
                 desktop.MainWindow = new MainWindow
                 {
--- VinLotteri/ViewModels/DrawingViewModel.cs
+++ VinLotteri/ViewModels/DrawingViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
+using System.Threading.Tasks;
 using ReactiveUI;
 using VinLotteri.Services;
 
@@ -8,11 +9,14 @@ namespace VinLotteri.ViewModels
 {
     public class DrawingViewModel : ViewModelBase
     {
-        int numberOfPrizes;
         private List<int> shuffleOrder;
         private List<int> winners;
+
+        private int progress;
         private IDatabase db;
         private IRandom randomService;
+
+        private bool isVisible;
         
         public DrawingViewModel(IDatabase db, IRandom randomService)
         {
@@ -20,20 +24,37 @@ namespace VinLotteri.ViewModels
             this.randomService = randomService;
             Draw = ReactiveCommand.Create(draw);
         }
-        
-        public int NumberOfPrizes
+
+        public bool IsVisible
         {
-            get => NumberOfPrizes;
-            set => this.RaiseAndSetIfChanged(ref numberOfPrizes, value);
+            get => isVisible;
+            set => this.RaiseAndSetIfChanged(ref isVisible, value);
+        }
+        
+        public int Progress
+        {
+            get => progress;
+            set => this.RaiseAndSetIfChanged(ref progress, value);
         }
 
         public ReactiveCommand<Unit, Unit> Draw { get; }
-
+ 
         private async void draw()
         {
+            IsVisible = false;
             var tickets = db.GetTickets().ToList();
-            shuffleOrder = await randomService.getRandomNumbers(1, tickets.Count, tickets.Count);
-            
+            await generateProgress();
+            IsVisible = true;
+            shuffleOrder = await randomService.getRandomNumbers(1, 10, 5);
+        }
+
+        private async Task generateProgress()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Progress = i;
+                await Task.Delay(15);
+            }
         }
     }
 }
